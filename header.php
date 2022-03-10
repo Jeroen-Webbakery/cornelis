71f77e62726d6bf67a4a621aa1dc4e2f6a18caa3
caspervanschooten <casper@webbakery.nl>
2022-03-10 13:08:09 +0100

    Add form add

--- header.php
+++ header.php
@@ -31,7 +31,24 @@ $container = get_theme_mod('understrap_container_type');
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-MRQ7VFC');</script>
+    <!-- Adform Tracking Code BEGIN -->
+    <script type="text/javascript">
+        window._adftrack = Array.isArray(window._adftrack) ? window._adftrack : (window._adftrack ? [window._adftrack] : []);
+        window._adftrack.push({
+            HttpHost: 'track.adform.net',
+            pm: 2596729,
+            divider: encodeURIComponent('|'),
+            pagename: encodeURIComponent('Conversie')
+        });
+        (function () { var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'https://s2.adform.net/banners/scripts/st/trackpoint-async.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x); })();
 
+    </script>
+    <noscript>
+        <p style="margin:0;padding:0;border:0;">
+            <img src="https://track.adform.net/Serving/TrackPoint/?pm=2596729&ADFPageName=Conversie&ADFdivider=|" width="1" height="1" alt="" />
+        </p>
+    </noscript>
+    <!-- Adform Tracking Code END -->
 
     <?php wp_head(); ?>
 </head>

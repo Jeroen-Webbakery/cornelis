47e9c8a566086d66bccfce90d36d0d92660d6297
caspervanschooten <casper@webbakery.nl>
2022-03-10 15:51:22 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -137,7 +137,7 @@
 
                         <div class="mb-2">
                             <?php if ( $bestand = get_sub_field( 'bestand', 'options' ) ) : ?>
-                            <a href="<?php esc_url( $bestand ); ?>" target="_blank"><?php the_sub_field('documentnaam', 'options'); ?><i class="fa-solid fa-file-arrow-down ml-2"></i> </a>
+                            <a href="<?= $bestand ?>" target="_blank"><?php the_sub_field('documentnaam', 'options'); ?><i class="fa-solid fa-file-arrow-down ml-2"></i> </a>
                             <p><?php the_sub_field('description', 'options'); ?></p>
                         <?php endif; ?>
                         </div>
